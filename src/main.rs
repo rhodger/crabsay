5caafe329bcd7f0a6985f7b6b533b847a3147b71
SuedeG <ymp17kwu@uea.ac.uk>
2020-08-17 13:24:28 +0100

    Added command line argument for length and some documentation + `-l` flag at command-line to specify length of lines + Documentation to `main.rs`

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,38 @@
+//! Main file for handling user input.
+//!
+//! All other logic is handled by other files.
+
 use clap::{App, Arg};
 
 mod crab;
 
+/// User input is handled using `clap`, which also generates help messages and
+/// version information.
 fn main() {
     let matches = App::new("crabsay")
-                    .version("0.1.0")
+                    .version("1.1.0")
                     .arg(Arg::with_name("text")
                             .index(1)
                             .required(true)
                             .help("input string"))
+                    .arg(Arg::with_name("length")
+                            .required(false)
+                            .short("l")
+                            .long("length")
+                            .default_value("40")
+                            .help("line length"))
                     .get_matches();
 
     let text = matches.value_of("text").unwrap();
 
-    println!("{}", crab::format(text, 80));
+    // Here the maximum length of each line of text is defined - it can be
+    // specified at the command-line by the user or left to default to 40.
+    let length: u8 = match matches.value_of("length") {
+        Some(x) => x.parse().unwrap_or(40),
+        None => 40
+    };
+
+    // This calls the necessary function from `crab.rs` to format the input
+    // correctly, the prints it out.
+    println!("{}", crab::format(text, length));
 }
